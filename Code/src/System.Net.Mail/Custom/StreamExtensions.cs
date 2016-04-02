8f1fb9d342c1123d6a8b6251f337e87704abec48
Josh Comley <joshcomley@googlemail.com>
2016-04-02 20:15:12 +0100

    Initial commit of port

---
+++ Code/src/System.Net.Mail/Custom/StreamExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace System
+{
+	//See: https://github.com/ServiceStack/ServiceStack/blob/master/src/ServiceStack.Client/AsyncUtils.cs
+	public static class StreamExtensions
+	{
+		public static IAsyncResult BeginWrite(this Stream stream, byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+		{
+			throw new NotImplementedException();
+		}
+		public static void EndWrite(this Stream stream, IAsyncResult result)
+		{
+			throw new NotImplementedException();
+		}
+		public static IAsyncResult BeginRead(this Stream stream, byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+		{
+			throw new NotImplementedException();
+		}
+		public static int EndRead(this Stream stream, IAsyncResult result)
+		{
+			throw new NotImplementedException();
+		}
+		public static void Close(this Stream stream)
+		{
+			stream.Dispose();
+		}
+	}
+}
---
+++ Code/src/System.Net.Mail/SR.cs
@@ -0,0 +1,60 @@
+﻿namespace System
+{
+	public class SR
+	{
+		public static string net_emptystringcall;
+		public static string SmtpOK;
+		public static string net_io_invalidasyncresult;
+		public static string net_io_invalidendcall;
+		public static string net_emptystringset;
+		public static string MailHeaderInvalidCID;
+
+		public static string GetString(string name, object name2 = null)
+		{
+			return name;
+		}
+
+		public static string MailDateInvalidFormat { get; set; }
+		public static string MailHeaderFieldInvalidCharacter { get; set; }
+		public static string InvalidHeaderName { get; set; }
+		public static string MailHeaderFieldMalformedHeader { get; set; }
+		public static string MimeTransferEncodingNotSupported { get; set; }
+		public static string SeekNotSupported { get; set; }
+		public static string ReadNotSupported { get; set; }
+		public static string WriteNotSupported { get; set; }
+		public static string MailAddressInvalidFormat { get; set; }
+		public static string MailServerResponse { get; set; }
+		public static string SmtpMustIssueStartTlsFirst { get; set; }
+		public static string SmtpClientNotPermitted { get; set; }
+		public static string SmtpTransactionFailed { get; set; }
+		public static string SmtpStartMailInput { get; set; }
+		public static string SmtpMailboxNameNotAllowed { get; set; }
+		public static string SmtpExceededStorageAllocation { get; set; }
+		public static string SmtpInsufficientStorage { get; set; }
+		public static string SmtpUserNotLocalTryAlternatePath { get; set; }
+		public static string SmtpLocalErrorInProcessing { get; set; }
+		public static string SmtpMailboxUnavailable { get; set; }
+		public static string SmtpMailboxBusy { get; set; }
+		public static string SmtpUserNotLocalWillForward { get; set; }
+		public static string SmtpServiceNotAvailable { get; set; }
+		public static string SmtpServiceClosingTransmissionChannel { get; set; }
+		public static string SmtpServiceReady { get; set; }
+		public static string SmtpHelpMessage { get; set; }
+		public static string SmtpSystemStatus { get; set; }
+		public static string SmtpCommandParameterNotImplemented { get; set; }
+		public static string SmtpBadCommandSequence { get; set; }
+		public static string SmtpCommandNotImplemented { get; set; }
+		public static string SmtpSyntaxError { get; set; }
+		public static string SmtpCommandUnrecognized { get; set; }
+		public static string SmtpNonAsciiUserNotSupported { get; set; }
+		public static string SmtpInvalidHostName { get; set; }
+		public static string InvalidHexDigit { get; set; }
+		public static string MailSubjectInvalidFormat { get; set; }
+		public static string MailWriterIsInContent { get; set; }
+		public static string MailBase64InvalidCharacter { get; set; }
+		public static string MimePartCantResetStream { get; set; }
+		public static string MediaTypeInvalid { get; set; }
+		public static string ContentTypeInvalid { get; set; }
+		public static string ContentDispositionInvalid { get; set; }
+	}
+}
